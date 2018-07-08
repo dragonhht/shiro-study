0c12df5af636799a965ea492599c8ad4e6676334
dragonhht <dragon_hht@163.com>
2018-07-08 19:32:11 +0800

    非注解配置为什么不生效

---
+++ src/main/kotlin/hht/dragon/shiro/config/ShiroConfig.kt
@@ -0,0 +1,109 @@
+package hht.dragon.shiro.config
+
+import hht.dragon.shiro.MyRealm
+import org.apache.shiro.SecurityUtils
+import org.apache.shiro.authc.credential.HashedCredentialsMatcher
+import org.apache.shiro.mgt.DefaultSecurityManager
+import org.apache.shiro.mgt.SecurityManager
+import org.apache.shiro.spring.LifecycleBeanPostProcessor
+import org.apache.shiro.spring.web.ShiroFilterFactoryBean
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.core.annotation.Order
+import org.apache.shiro.spring.security.interceptor.AuthorizationAttributeSourceAdvisor
+
+
+
+
+/**
+ * Shiro配置类.
+ * User: huang
+ * Date: 18-7-8
+ */
+@Configuration
+class ShiroConfig {
+
+    /**
+     * Shiro过滤器.
+     */
+    @Bean
+    fun shiroFilter(securityManager: SecurityManager) : ShiroFilterFactoryBean {
+        val factoryBean = ShiroFilterFactoryBean()
+
+        // 设置SecurityManager
+        factoryBean.securityManager = securityManager
+        // 设置登录界面请求，不设置则默认访问根目录下的"/login.jsp"
+        factoryBean.loginUrl = "/static/login.html"
+        // 设置登录成功后的跳转链接
+        factoryBean.successUrl = "/index"
+        // 设置未授权界面
+        factoryBean.unauthorizedUrl = "/403"
+
+        // 拦截器
+        val filterChainDefinitionMap = LinkedHashMap<String, String>()
+
+        // 配置不会被拦截的链接,按顺序判断
+        // anon ：  请求可匿名访问
+        filterChainDefinitionMap["/static/**"] = "anon"
+        filterChainDefinitionMap["/login"] = "anon"
+
+        // 配置退出过滤器,其中的具体的退出代码Shiro已经替我们实现了
+        filterChainDefinitionMap["/logout"] = "logout"
+
+        filterChainDefinitionMap["/index"] = "authc"
+        // 拦截器会按配置从上往下检查，所以"/**"请求放在最后
+        // authc : 所有url都必须认证通过才可以访问
+        filterChainDefinitionMap["/**"] = "authc,roles[user]"
+
+        factoryBean.filterChainDefinitionMap = filterChainDefinitionMap
+        println("Shiro拦截器配置完毕")
+
+        SecurityUtils.setSecurityManager(securityManager)
+
+        return factoryBean
+    }
+
+    /**
+     * 配置SecurityManager.
+     */
+    @Bean
+    fun securityManager() : SecurityManager {
+        val manager = DefaultSecurityManager()
+        manager.setRealm(getRealm())
+        return manager
+    }
+
+    /**
+     * 配置自定义Realm.
+     */
+    @Bean
+    fun getRealm() : MyRealm {
+        val realm = MyRealm()
+        realm.credentialsMatcher = hashedCredentialsMatcher()
+        return realm
+    }
+
+    /**
+     * 配置加密.
+     */
+    @Bean
+    fun hashedCredentialsMatcher() : HashedCredentialsMatcher {
+        val matcher = HashedCredentialsMatcher()
+        matcher.hashAlgorithmName = "md5"
+        matcher.hashIterations = 1
+        return matcher
+    }
+
+    @Bean
+    fun lifecycleBeanPostProcessor(): LifecycleBeanPostProcessor {
+        return LifecycleBeanPostProcessor()
+    }
+
+    @Bean
+    fun authorizationAttributeSourceAdvisor(securityManager: SecurityManager): AuthorizationAttributeSourceAdvisor {
+        val authorizationAttributeSourceAdvisor = AuthorizationAttributeSourceAdvisor()
+        authorizationAttributeSourceAdvisor.securityManager = securityManager
+        return authorizationAttributeSourceAdvisor
+    }
+
+}
---
+++ src/main/kotlin/hht/dragon/shiro/controller/IndexController.kt
@@ -0,0 +1,60 @@
+package hht.dragon.shiro.controller
+
+import org.apache.shiro.SecurityUtils
+import org.apache.shiro.authc.UsernamePasswordToken
+import org.apache.shiro.authz.annotation.RequiresRoles
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+import reactor.core.publisher.Mono
+
+/**
+ * Controller.
+ * User: huang
+ * Date: 18-7-8
+ */
+@RestController
+class IndexController {
+
+    /**
+     * 首页.
+     */
+    @GetMapping("/index")
+    fun index() : Mono<String> {
+        return Mono.just("首页")
+    }
+
+    /**
+     * 模拟登录界面请求.
+     */
+    @GetMapping("/login")
+    fun login(userName: String, password: String) : Mono<String> {
+        val subject = SecurityUtils.getSubject()
+
+        val token = UsernamePasswordToken(userName, password)
+        return try {
+            subject.login(token)
+            Mono.just("登录成功")
+        } catch (e : Exception) {
+            e.printStackTrace()
+            Mono.just("登录失败")
+        }
+    }
+
+    /**
+     * 模拟跳转未授权界面.
+     */
+    @GetMapping("/403")
+    fun notAuth() : Mono<String> {
+        return Mono.just("403")
+    }
+
+    /**
+     * 使用注解设定访问权限角色
+     */
+    @RequiresRoles("admin")
+    @GetMapping("/test")
+    fun test() : Mono<String> {
+        return Mono.just("测试注解")
+    }
+
+}
